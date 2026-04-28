5bb077fd40ab097a6910f2877e1261d0cb28df0a
djumanov <djumanovdev@gmail.com>
2026-04-28 18:49:35 +0500

    template engine

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+posts = [
+    {
+        "id": 1,
+        "title": "Post 01",
+        "content": "Post data",
+    },
+    {
+        "id": 2,
+        "title": "Post 02",
+        "content": "Post data",
+    },
+    {
+        "id": 3,
+        "title": "Post 03",
+        "content": "Post data",
+    },
+    {
+        "id": 4,
+        "title": "Post 04",
+        "content": "Post data",
+    },
+]
+
 
 def home_view(request: HttpRequest) -> HttpResponse:
-    return render(request, 'home.html')
+    name = 'Ali Valiyev - Blog Site'
+    return render(
+        request,
+        'home.html',
+        context={
+            'name': name,
+            'posts': posts[-3:]
+        },
+    )
 
 
 def blogs_view(request: HttpRequest) -> HttpResponse:
