f7bbce483439f61175a50c865095bc0f2c6168f7
djumanov <djumanovdev@gmail.com>
2026-04-30 18:31:59 +0500

    context data

---
+++ blog/db.py
@@ -0,0 +1,31 @@
+import json
+
+
+DEMO_DB_PATH = "db.json"
+
+class Database:
+    def __init__(self, path=DEMO_DB_PATH):
+        self.path = path
+
+    def read(self):
+        with open(self.path, "r") as f:
+            return json.load(f)
+
+    def write(self, data):
+        with open(self.path, "w") as f:
+            json.dump(data, f, indent=4)
+
+    def get_articles(self):
+        data = self.read()
+        return data.get("articles", [])
+    
+    def get_latest_articles(self, count=3):
+        articles = self.get_articles()
+        return articles[:count]
+    
+    def get_article_by_slug(self, slug):
+        articles = self.get_articles()
+        for article in articles:
+            if article.get("slug") == slug:
+                return article
+        return None
