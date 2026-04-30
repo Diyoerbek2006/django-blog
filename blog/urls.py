89565ddd8afbc437966711a6e8cfec4b7a043abd
djumanov <djumanovdev@gmail.com>
2026-04-30 18:00:13 +0500

    css

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('articles/', views.articles_page, name='articles'),
     path('about/', views.about_page, name='about'),
-    path('contact/', views.about_page, name='contact'),
+    path('contact/', views.contact_page, name='contact'),
 ]
